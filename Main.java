1e034ad5204e75c29345637946285e6e9a7afee1
Aleksandr <alexlev83@list.ru>
2023-12-22 13:46:16 +0300

    Temp

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Product> productList = new ArrayList<>();
-        productList.add(new HotDrinks("Coffee", 200, 90));
-        productList.add(new HotDrinks("Tea", 200, 90));
+        productList.add(new HotDrinks("Coffee", 200, 1,90));
+        productList.add(new HotDrinks("Tea", 200,1, 90));
         HotDrinksVendingMachine vendingMachine = new HotDrinksVendingMachine();
         vendingMachine.initProduct(productList);
         System.out.println(vendingMachine.getProduct("Coffee", 90));
-        Product test = new HotDrinks("Coffee", 200, 90);
+        System.out.println(vendingMachine.getProduct("Tea", 90));
     }
 }
